b0a4e10acd681fb140b9971579e15d32ffe84c84
Ian Salt <redearthbluesky@yahoo.co.uk>
2014-11-28 09:36:06 +0000

    Friday morning commit

--- compass/config.rb
+++ compass/config.rb
@@ -8,13 +8,15 @@ images_dir = "../images"    #the folder with your images
 javascript_dir = "../js"    #folder where js files are
 fonts_dir = "../fonts"      #folder where font files are
 
+ environment = :development
+ #environment = :production
 
 # You can select your preferred output style here (can be overridden via the command line):
 output_style = :expanded # After dev :compressed
 
 # In development, we can turn on the FireCompass-compatible debug_info.
-#firecompass = false
-firecompass = true
+firecompass = false
+#firecompass = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = true
@@ -24,7 +26,8 @@ preferred_syntax = :scss
 
 # Pass options to sass. For development, we turn on the FireCompass-compatible
 # line_comments if the firecompass config variable above is true.
-sass_options = {:debug_info => true}
+-sass_options = {:debug_info => true}
++sass_options = {:line_comments => true}
 
 
 
--- js/main.js
+++ js/main.js
@@ -2,9 +2,63 @@
 
 $(document).ready(function () {
 
+  /*  hide and show the comments panel upon the creation of a new post */
+  var varbs = {
+    buttonGroup:    "aside#bets #banter .new-post-wrapper .btn-group",
+    eb: "#emailBanter",
+    newPostButton: '.player-wrapper button'
+  }
+
+  $(varbs.newPostButton).click(function () {
+    $('#bets h1, #bets table, #commets-wrapper').slideToggle();
+    /* hide the button group as this will not work on the document load */
+    $(varbs.buttonGroup).hide();
+    /*  make sure the check box is not checked */
+    $(varbs.eb).prop('checked', false);
+  });
+
+  /*  hide the email list and display it when the checkbox is selected */
+  $('#emailBanter').on('change', (function () {
+    if ($(this).is(':checked')) {
+      $(varbs.buttonGroup).show("500");
+    } else {
+      $(varbs.buttonGroup).hide("500");
+      /*  clear all selections from the list */
+      $('#player-email-list').multiselect("clearSelection");
+    }
+  }));
+
+
+/*  Configuration for the multiselect plugin */
+  $('#player-email-list').multiselect({
+    includeSelectAllOption: true,
+    selectAllText: 'SELECT ALL PLAYERS!',
+    nonSelectedText: 'Select players to email banter',
+    allSelectedText: 'All players selected',
+    maxHeight: 200
+  });
+
+  /*  switch panel content to specific week on select drop down */
+  $("select#scoreRounds").change(function () {
+    var value = $("select#scoreRounds option:selected").attr("value");
+    value = "." + value;
+    $("#box-wrapper div[class^=\"week\"]").hide();
+    $(value).show(500);
+  }).change();
+  
+  /*  switch panel content to specific league on select drop down */
+  $("select#banterLeague").change(function () {
+    var value = $("select#banterLeague option:selected").attr("value");
+    value = "." + value;
+    $("#banterWrapper div[class^=\"league\"]").hide();
+    $(value).show(500);
+  }).change();
+
+
+
 /*  detect the window height and set the players element to it */
     function setHeight() {
-      windowHeight = ($(window).innerHeight() - 300);
+      windowHeight = ($(window).innerHeight() - 400);
       $('#players').css('max-height', windowHeight);
     };
     setHeight();
